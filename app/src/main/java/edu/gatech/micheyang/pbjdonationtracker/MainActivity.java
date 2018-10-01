c3b090e82579f7869bf4d9bcd9dd913e09b7f03d
rosie <rosieblair@gatech.edu>
2018-10-01 14:23:44 -0400

    Added explicit Button var's + onClick() methods to MainActivity
    Explicit onClick() methods implemented for button presses

--- app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/MainActivity.java
+++ app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/MainActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button logButton;
+    private Button regButton;
 
     /***
      * Method that creates the activity when it is launched.
@@ -17,27 +21,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pressLogin();
+        pressRegister();
     }
 
-    /***
-     * The method called when a user requests to log-in to application w/ "LOGIN"
-     *
-     * @param view the selected view
-     */
-    public void onLoginPressed(View view) {
-        Log.d("Edit", "Log in");
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+    public void pressLogin() {
+        logButton = (Button) findViewById(R.id.login);
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("edu.gatech.micheyang.pbjdonationtracker.LoginActivity");
+                startActivity(intent);
+            }
+        });
     }
 
-    /***
-     * The method called when a user requests to make a new account w/ "REGISTER"
-     *
-     * @param view the selected view
-     */
-    public void onRegisterPressed(View view) {
-        Log.d("Edit", "Log in");
-        Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-        startActivity(intent);
+    public void pressRegister() {
+        regButton = (Button) findViewById(R.id.register);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("edu.gatech.micheyang.pbjdonationtracker.RegistrationActivity");
+                startActivity(intent);
+            }
+        });
+
     }
 }
+
+
+//    /***
+//     * The method called when a user requests to log-in to application w/ "LOGIN"
+//     *
+//     * @param view the selected view
+//     */
+//    public void onLoginPressed(View view) {
+//        Log.d("Edit", "Log in");
+//        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    /***
+//     * The method called when a user requests to make a new account w/ "REGISTER"
+//     *
+//     * @param view the selected view
+//     */
+//    public void onRegisterPressed(View view) {
+//        Log.d("Edit", "Log in");
+//        Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+//        startActivity(intent);
+//    }
+//}
