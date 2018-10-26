d103bf64dfa879fac0d71fd7492c769aa19e632e
rosie <rosieblair@gatech.edu>
2018-10-26 18:42:32 -0400

    created new User class with get/set's for each of its attributes to use with DB

---
+++ app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/SQLDatabase/User.java
@@ -0,0 +1,51 @@
+package edu.gatech.micheyang.pbjdonationtracker.SQLDatabase;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private boolean locked;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean getLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+}
