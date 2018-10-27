2f42dc8a3111a2d6bfea3ec668a254953425b696
rosie <rosieblair@gatech.edu>
2018-10-27 02:42:15 -0400

    created a controller class to handle user inputs
    Can implement ideas noted in it later on for extra features. Used this to test input for valid args (i.e. check input != null).

---
+++ app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/controllers/ValidateUserInput.java
@@ -0,0 +1,24 @@
+package edu.gatech.micheyang.pbjdonationtracker.controllers;
+
+import android.content.Context;
+
+
+public class ValidateUserInput {
+
+    private Context context;
+
+    public ValidateUserInput(Context context) {
+        this.context = context;
+    }
+
+    /*
+    this class will be used to validate or check user inputs.
+    we can implement later on for extra credit.
+    ideas to implement:
+        -   Check if user inputs a null argument.
+        -   Check if email address is valid (e.g. '@', '.com', etc).
+        -   Check if password meets requirements (enhance security).
+        -   Check if username contains invalid characters (e.g. '$', '*', etc).
+        -   Check if 2 inputs match (security or things like confirm email).
+     */
+}
