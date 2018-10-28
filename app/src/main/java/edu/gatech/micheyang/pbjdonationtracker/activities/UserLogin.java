9725fab5b6e7926b05f37dadd75d0c19c1c14ca9
rosie <rosieblair@gatech.edu>
2018-10-28 15:32:40 -0400

    made copies of LoginActivity + XML files to use for db
    made to test prior login functionality using user info stored in sql database

--- app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/activities/UserLogin.java
+++ app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/activities/UserLogin.java
@@ -2,18 +2,16 @@ package edu.gatech.micheyang.pbjdonationtracker.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import edu.gatech.micheyang.pbjdonationtracker.AppScreen;
+import edu.gatech.micheyang.pbjdonationtracker.MainActivity;
 import edu.gatech.micheyang.pbjdonationtracker.R;
-import edu.gatech.micheyang.pbjdonationtracker.SQLDatabase.DatabaseHelper;
+import edu.gatech.micheyang.pbjdonationtracker.database.DatabaseHelper;
 
 
 public class UserLogin extends AppCompatActivity implements View.OnClickListener {
@@ -27,20 +25,56 @@ public class UserLogin extends AppCompatActivity implements View.OnClickListener
     private Button buttonCancel;
     private TextView failedLogin;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+        init();
+    }
 
+    private void init() {
+        dbhelper = new DatabaseHelper(activity);
 
+        inputUsername = findViewById(R.id.enterLoginUsername);
+        inputPassword = findViewById(R.id.enterLoginPassword);
 
+        failedLogin = findViewById(R.id.invalidNotification);
 
-
-
-
-
-
-
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonCancel = findViewById(R.id.buttonCancelLogin);
+        buttonLogin.setOnClickListener(this);
+        buttonCancel.setOnClickListener(this);
+    }
 
 
     @Override
     public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.buttonCancelLogin:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                resetFields();
+                startActivity(intent);
+                break;
+            case R.id.buttonLogin:
+                validate();
+                break;
+        }
+    }
 
+    private void validate() {
+        String username = inputUsername.getText().toString();
+        String password = inputPassword.getText().toString();
+        if (dbhelper.checkUserPass(username, password)) {
+            Intent intent = new Intent(activity, AppScreen.class);
+            resetFields();
+            startActivity(intent);
+        } else {
+            failedLogin.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void resetFields() {
+        inputUsername.setText(null);
+        inputPassword.setText(null);
     }
 }
