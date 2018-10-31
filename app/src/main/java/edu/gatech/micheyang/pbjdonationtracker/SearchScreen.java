ae7dfccbafb34b652906cef5a4f4f4c95ac3ef08
Sabrina Wilson <sawilson3011@gmail.com>
2018-10-31 08:32:55 -0400

    Updated SearchScreen so pressing search button goes to list of locations screen

--- app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/SearchScreen.java
+++ app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/SearchScreen.java
@@ -1,8 +1,6 @@
 package edu.gatech.micheyang.pbjdonationtracker;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,9 +11,9 @@ import android.content.Intent;
 
 public class SearchScreen extends AppCompatActivity {
 
-    private EditText item;            // item input
-    private Spinner category;         // category spinner
-    private Spinner location;         // location spinner
+//    private EditText item;            // item input
+//    private Spinner category;         // category spinner
+//    private Spinner location;         // location spinner
     private Button searchForItem;     // item search button
     private Button searchForCat;      // category search button
     private Button searchForLoc;      // location search button
@@ -32,44 +30,35 @@ public class SearchScreen extends AppCompatActivity {
         pressSearchLoc();
     }
 
-    /**
-     *
-     */
     public void pressSearchItem() {
         searchForItem = (Button) findViewById(R.id.itemSearchButton);
         searchForItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent("edu.gatech.micheyang.pbjdonationtracker.AddItemScreen");
-                //startActivity(intent);
+                Intent intent = new Intent("edu.gatech.micheyang.pbjdonationtracker.ListOfLocations");
+                startActivity(intent);
             }
         });
     }
 
-    /**
-     *
-     */
     public void pressSearchCat() {
         searchForCat = (Button) findViewById(R.id.catSearchButton);
         searchForCat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent("edu.gatech.micheyang.pbjdonationtracker.AddItemScreen");
-                //startActivity(intent);
+                Intent intent = new Intent("edu.gatech.micheyang.pbjdonationtracker.ListOfLocations");
+                startActivity(intent);
             }
         });
     }
 
-    /**
-     *
-     */
     public void pressSearchLoc() {
         searchForLoc = (Button) findViewById(R.id.locSearchButton);
         searchForLoc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent("edu.gatech.micheyang.pbjdonationtracker.AddItemScreen");
-                //startActivity(intent);
+                Intent intent = new Intent("edu.gatech.micheyang.pbjdonationtracker.ListOfLocations");
+                startActivity(intent);
             }
         });
     }
