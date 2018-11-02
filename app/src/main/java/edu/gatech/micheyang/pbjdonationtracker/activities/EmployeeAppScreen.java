1007624a814c577897b8e414ba69956ecd6e54ac
rosie <rosieblair@gatech.edu>
2018-11-02 19:10:43 -0400

    made new employee app screen in activities package for use with DB
    Need to implement this with methods to view and update inventory of the particular location that the employee works at

---
+++ app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/activities/EmployeeAppScreen.java
@@ -0,0 +1,13 @@
+package edu.gatech.micheyang.pbjdonationtracker.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeAppScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_screen);
+    }
+}
