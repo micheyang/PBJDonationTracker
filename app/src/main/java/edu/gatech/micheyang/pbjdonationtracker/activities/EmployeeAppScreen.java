5e45912bb379b6ccf21080b6f57409c4bdd1251a
rosie <rosieblair@gatech.edu>
2018-11-02 21:10:47 -0400

    created copy of employee app screen to be used with info from db

--- app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/activities/EmployeeAppScreen.java
+++ app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/activities/EmployeeAppScreen.java
@@ -3,6 +3,8 @@ package edu.gatech.micheyang.pbjdonationtracker.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import edu.gatech.micheyang.pbjdonationtracker.R;
+
 public class EmployeeAppScreen extends AppCompatActivity {
 
     @Override
